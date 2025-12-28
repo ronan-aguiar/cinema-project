9efadedfbff4501410a21053f5fe5e303b889a01
ronanaguiar <ronan.agf@gmail.com>
2025-12-27 22:39:48 -0300

    Configura deploy: ALLOWED_HOSTS e STATIC_ROOT

--- cinema_backend/settings.py
+++ cinema_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o@0%js!=j@lv$dfo!()z16vs4l9&=iys@g6w@$yiu5-4x&ie!p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,3 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
